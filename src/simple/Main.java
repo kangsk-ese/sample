9dba6be7e5acd318040105941c426c944a26031f
osguli <osguli.sk@gmail.com>
2018-04-28 11:32:20 +0900

    commit for modified file

--- src/simple/Main.java
+++ src/simple/Main.java
@@ -2,9 +2,8 @@ package simple;
 
 public class Main {
 
-	public static void main(String[] args)
-	{
-		System.out.println("Hello");	
-	//hello world!	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// hello world!
 	}
 }
